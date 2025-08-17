bb39f65faeb523efcc6805deb88ec51b8fac1cb0
Nickspero <nspappas99@gmail.com>
2025-08-17 12:58:54 -0400

    Add files via upload

---
+++ indexE-com.js
@@ -0,0 +1,9 @@
+//this is the functionality of the hamburger menu//
+
+function openMenu() {
+    document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
